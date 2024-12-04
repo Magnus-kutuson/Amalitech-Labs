f66ca01a5f607c38124a9d0ceb246e5baa5296fd
Magnus-kutuson <magnus.kutuson@amalitech.com>
2024-12-04 10:55:41 +0000

    donw with the forms

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
@@ -7,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './forms.component.css'
 })
 export class FormsComponent {
-
+protected name = new FormControl<string>('', Validators.required)
+protected email = new FormControl<any>('', Validators.required)
+protected phone = new FormControl<string>('', Validators.required)
 }
