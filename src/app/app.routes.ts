e20198b45762cec6fc556c00263c74712a524069
Magnus-kutuson <magnus.kutuson@amalitech.com>
2024-12-05 08:19:35 +0000

    started routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,12 @@
-import { Routes } from '@angular/router';
+import { Routes, Route} from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { FormsComponent } from './forms/forms.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HeaderComponent, title: 'Your info', 
+        children: [
+        { path: 'step1', component: FormsComponent, title: 'Your info' },
+    ]},
+
+];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsComponent } from '../forms/forms.component';
 import { ButtonComponent } from '../button/button.component';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [FormsComponent, ButtonComponent],
+  imports: [FormsComponent, ButtonComponent, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
