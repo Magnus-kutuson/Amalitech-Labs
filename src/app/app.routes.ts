801c3c9c90e880b88d451ff2e38b812bfa27463f
Magnus-kutuson <magnus.kutuson@amalitech.com>
2024-12-05 09:33:38 +0000

    finished the select plan page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes, Route} from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FormsComponent } from './forms/forms.component';
-import { AppComponent } from './app.component';
+import { OptionsComponent } from './options/options.component';
 
 export const routes: Routes = [
     { path: '', component: HeaderComponent, title: 'Your info', 
         children: [
         { path: 'step1', component: FormsComponent, title: 'Your info' },
+        { path: 'step2', component: OptionsComponent, title: 'Select plan' },
     ]},
 
 ];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { FormsComponent } from '../forms/forms.component';
 import { ButtonComponent } from '../button/button.component';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { OptionsComponent } from '../options/options.component';
 
 @Component({
   selector: 'app-header',
-  imports: [FormsComponent, ButtonComponent, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [FormsComponent, ButtonComponent, RouterOutlet, RouterLink, RouterLinkActive, OptionsComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
